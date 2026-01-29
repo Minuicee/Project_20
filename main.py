83bcdcb82dea8e2bdf69dcd7088371e6e099a747
Minuicee <jasonlembcke@gmail.com>
2026-01-29 17:27:29 +0100

    start implementing ai 2

--- main.py
+++ main.py
@@ -1,39 +1,41 @@
 import matplotlib.pyplot as plt
 import torch.nn as nn
+import torch
 
-class Main:
+class AI:
 
-    def __init__(self, folder, sigma):
-        self.get_data(folder)
+    def __init__(self, folder, sigma, focused_unit):
+        self.sigma = sigma
+        self.focused_unit = focused_unit
 
-
-    def get_data(self, folder):
         # get info from info csv
-        with open(f"{folder}/sets/info.csv") as f:
-            unit_lengths = f.readline().strip().split(",")
-            self.unit_lengths = [int(x) for x in unit_lengths]    
-            self.filter_list = f.readline().strip()
+        with open(f"sets/{folder}/info.csv") as f:
+            self.unit_lengths = [int(x) for x in f.readline().strip().split(",")] 
 
         # init target language
-        with open(f"{folder}/sets/target.csv", "r") as f:
-            self.target = [line for line in f]
+        with open(f"sets/{folder}/target.csv", "r") as f:
+            self.target = [line.strip().lower() for line in f]
         
         # init coresponding source language translation from data
-        with open(f"{folder}/sets/source.csv", "r") as f:
-            self.source = [self.filter(line) for line in f]
+        with open(f"sets/{folder}/source.csv", "r") as f:
+            self.source = [line.strip().lower() for line in f]
 
 
-    def learn(self):
-        print(self.target)
+    def get_word(self):
+        unit_weights = self.gauss_distribution()
+        plt.plot(unit_weights)
+        plt.show()
 
+    def is_correct(self, input, target):
+        return True if input in target and len(input) > 1 else False
     
-    def filter(self, word):
-        for c in self.filter_list:
-            word = word.replace(c, "")
-        return word.lower()
-    
+    def gauss_distribution(self):
+        indices = torch.arange(len(self.unit_lengths), dtype=torch.float32)
+        return torch.exp(-0.5*((indices - self.focused_unit)/self.sigma)**2)
 
 
 # run main
-application = Main("german-latin", 1)
-application.learn()
+model = AI(folder="german-latin",
+                 sigma=1,
+                 focused_unit=5)
+model.get_word()
